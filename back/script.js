391f122b407eec4a2e2e837c6235ad4e79f68340
SamuelCFalcao <samuelfalcao@alu.ufc.br>
2025-10-16 17:07:24 -0300

    .

--- back/script.js
+++ back/script.js
@@ -1,4 +1,4 @@
-function validaFormulario(event) {
+function validarFormulario(event) {
   event.preventDefault();
 
   const campoNome = document.getElementById("nome");
@@ -6,11 +6,13 @@ function validaFormulario(event) {
   const divMensagem = document.getElementById("mensagem");
 
   if (valorNome === "") {
+    alert("Erro: Por favor, preencha o campo nome.");
     divMensagem.className = "mensagem erro";
     divMensagem.textContent = "Por favor, preencha o campo nome.";
     divMensagem.style.display = "block";
     return false;
   } else {
+    alert("Sucesso: Nome enviado com sucesso!");
     divMensagem.className = "mensagem sucesso";
     divMensagem.textContent = "Nome enviado com sucesso!";
     divMensagem.style.display = "block";
